3ca58b186aa457ddcc9cbd2a28811708bed2ec43
santosh-karki <email@santoshkarki930@gmail.com>
2024-07-27 15:48:34 +1000

    commits

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -15,7 +15,7 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
     
-    const response = await fetch('/api/workouts', {
+    const response = await fetch('https://mern-tr9g.onrender.com/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
